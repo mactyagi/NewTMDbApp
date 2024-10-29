4f73d42f03b2bb5bdb83107720cb77be2513375f
Manukant Tyagi <mactyagi@icloud.com>
2024-10-29 13:54:07 +0530

    Initial Commit

---
+++ NewTMDbApp/NewTMDbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTMDbAppApp.swift
+//  NewTMDbApp
+//
+//  Created by manukant tyagi on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewTMDbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
